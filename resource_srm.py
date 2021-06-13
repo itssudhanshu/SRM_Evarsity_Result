3033439d973697131ae766cf5019a06a8e3316e9
itssudhanshu <sudhanshu_kushwaha@srmuniv.edu.in>
2021-06-13 18:03:25 +0530

    test36

--- resource_srm.py
+++ resource_srm.py
@@ -4,56 +4,23 @@ except ImportError:
     import Image
 import io
 import pytesseract
-# import cv2
-# import glob
-# import numpy
-# import ntpath
 import time
 import requests
 from bs4 import BeautifulSoup
 
-
-# UPLOAD_FOLDER = 'static/uploads/'
-# symbols = glob.glob(UPLOAD_FOLDER+'*.jpg')
 # pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
+# pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
 # pytesseract.pytesseract.tesseract_cmd = '/app/vendor/tesseract-ocr/bin/tesseract'
 
 captcha_url="https://evarsity.srmist.edu.in/srmwebonline/Captcha"
 name=int(round(time.time() * 1000))   
 
 
-# def guess_captcha(image):
-#     image = Image.open(image)
-#     pixels = image.load()
-#     size = image.size
-
-#     # Cleanup background noises from captcha
-#     for x in range(size[0]):
-#         for y in range(size[1]):
-#             if pixels[x, y][0] < 120:
-#                 pixels[x, y] = (0, 0, 0)
-#             else:
-#                 pixels[x, y] = (255, 255, 255)
-
-#     # Search symbols in captcha
-#     image = numpy.array(image)
-#     result = []
-#     for symbol in symbols:
-#         img_symbol = cv2.imread(symbol)
-#         match = cv2.matchTemplate(img_symbol, image, cv2.TM_CCOEFF_NORMED)
-#         if len(match):
-#             _, quality, _, location = cv2.minMaxLoc(match)
-#             if quality > 0.8:
-#                 result.append({'x': location[0], 'symbol': ntpath.basename(symbol).replace('.jpg', '')})
-#     result = sorted(result, key=lambda k: k['x'])
-#     return ''.join([x['symbol'] for x in result])
-
-
 def ocr_core(filename):
     """
     This function will handle the core OCR processing of images.
     """
+    pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
     text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
     # text = guess_captcha(filename)
 
